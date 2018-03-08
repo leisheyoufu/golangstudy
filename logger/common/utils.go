d8daa28958991ec52ab56e25fbce1a5aadaaddb1
leisheyoufu <clscu@live.cn>
2018-03-08 16:00:54 +0800

    Add socket exercise

---
+++ logger/common/utils.go
@@ -0,0 +1,40 @@
+package common
+
+import (
+	"runtime"
+	"fmt"
+	"path/filepath"
+	"os"
+	"log"
+	"strings"
+)
+
+func Logger() {
+	_, filepath, line, _ := runtime.Caller(1)
+	str1 := getCurrentDirectory()
+
+	//str2 := getParentDirectory(str1)
+	fmt.Println(str1)
+	fmt.Println(filepath, line)
+}
+
+func getCurrentDirectory() string {
+	dir, err := filepath.Abs(filepath.Dir(os.Args[0]))
+	if err != nil {
+		log.Fatal(err)
+	}
+	return strings.Replace(dir, "\\", "/", -1)
+}
+
+func substr(s string, pos, length int) string {
+	runes := []rune(s)
+	l := pos + length
+	if l > len(runes) {
+		l = len(runes)
+	}
+	return string(runes[pos:l])
+}
+
+func getParentDirectory(dirctory string) string {
+	return substr(dirctory, 0, strings.LastIndex(dirctory, "/"))
+}
