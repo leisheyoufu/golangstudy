f6263c09d1d219b36104cce4b02c561af86de152
leisheyoufu <clscu@live.cn>
2018-03-12 16:59:28 +0800

    Use  `()=>{}` function instead of bind

--- webterm/frontend/src/js/index.js
+++ webterm/frontend/src/js/index.js
@@ -7,19 +7,19 @@ class Session {
         this.ws = new WebSocket(this.url);
         this.term = this.openTerm();
         console.log(this.term);
-        this.ws.onmessage = function(event) {
+        this.ws.onmessage = (event) => {
             this.term.write(event.data);
-        }.bind(this);
-        this.term.on('key', function(key, ev) {
+        };
+        this.term.on('key', (key, ev) => {
             this.term.fit();
             this.ws.send(key);
-        }.bind(this));
+        });
     }
     openTerm() {
         var terminalContainer = document.getElementById('term-box');
         var term = new Terminal({
             cursorBlink: true
-        })
+        });
         term.open(terminalContainer)
         term.fit();
         return term;
