022def3641c809f794d0c1f063200a9182275f7d
leisheyoufu <clscu@live.cn>
2023-05-04 16:19:00 +0800

    remove accent from string

--- base/utils.go
+++ base/utils.go
@@ -1,10 +1,15 @@
 package base
 
 import (
+	"fmt"
+	"golang.org/x/text/runes"
+	"golang.org/x/text/transform"
+	"golang.org/x/text/unicode/norm"
 	"math"
 	"math/rand"
 	"reflect"
 	"time"
+	"unicode"
 )
 
 func ShuffleArray(arr interface{}) {
@@ -25,4 +30,13 @@ func round(num float64) int {
 func Float64ToFixed(num float64, precision int) float64 {
 	output := math.Pow(10, float64(precision))
 	return float64(round(num*output)) / output
+}
+
+// Removing accents from strings
+func ExampleRemove() {
+	t := transform.Chain(norm.NFD, runes.Remove(runes.In(unicode.Mn)), norm.NFC)
+	s, _, _ := transform.String(t, "résumé")
+	fmt.Println(s)
+	// Output:
+	// resume
 }
